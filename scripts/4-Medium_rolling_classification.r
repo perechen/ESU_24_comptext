be004a7e494b1c7c687e843db3de570a58dec62b
remolek <jeremi.ochab@gmail.com>
2024-07-22 15:30:53 +0300

    Update 4-Medium_rolling_classification.r

--- scripts/4-Medium_rolling_classification.r
+++ scripts/4-Medium_rolling_classification.r
@@ -11,6 +11,8 @@ rolling.classify(classification.method = "svm", write.png.file = TRUE)
 # In fact there is a number of parameters you can control, go ahead and try changing some of them, you can find all options in CRAN documentation: https://cran.r-project.org/web/packages/stylo/stylo.pdf or by checking
 help(rolling.classify) 
 
+# training.set.sampling is set to no sampling by default.
+# slice.size overrides controls the sample.size 
 results = rolling.classify(write.png.file = TRUE, classification.method = "nsc", 
                  mfw=50, training.set.sampling = "normal.sampling", 
                  slice.size = 5000, slice.overlap = 4500,
